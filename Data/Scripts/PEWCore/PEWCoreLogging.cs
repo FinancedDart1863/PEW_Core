988d534a3182500a93788a756b2a38ea5b289ac9
FinancedDart1863 <112011102+FinancedDart1863@users.noreply.github.com>
2023-02-16 23:34:52 -0500

    Implement PEW core logging system
    Implement the core logging mechanisms to be used by PEW core. Writes a log file to %AppData%\Roaming\SpaceEngineers\Storage\ that is searchable by the mod workshop number and mod name.

---
+++ Data/Scripts/PEWCore/PEWCoreLogging.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.ModAPI;
+
+namespace PEWCore
+{
+    public class PEWCoreLogging
+    {
+        private static PEWCoreLogging PEWCoreLoggingReference;
+
+        private TextWriter PEWCoreWriter = null;
+        private int indent = 0;
+        private StringBuilder cache = new StringBuilder();
+
+        static public PEWCoreLogging Instance
+        {
+            get
+            {
+                if (MyAPIGateway.Utilities == null)
+                    return null;
+
+                if (PEWCoreLoggingReference == null)
+                    PEWCoreLoggingReference = new PEWCoreLogging("PEWCore.log");
+
+                return PEWCoreLoggingReference;
+            }
+        }
+
+        public PEWCoreLogging(string logFile)
+        {
+            try
+            {
+                if (MyAPIGateway.Utilities != null)
+                    PEWCoreWriter = MyAPIGateway.Utilities.WriteFileInLocalStorage(logFile, typeof(PEWCoreLogging));
+
+                PEWCoreLoggingReference = this;
+            }
+            catch { }
+        }
+
+        public void IncreaseIndent()
+        {
+            indent++;
+        }
+
+        public void DecreaseIndent()
+        {
+            if (indent > 0)
+                indent--;
+        }
+
+        public void WriteLine(string text)
+        {
+            if (PEWCoreWriter == null)
+                return;
+
+            if (cache.Length > 0)
+                PEWCoreWriter.WriteLine(cache);
+
+            cache.Clear();
+            cache.Append(DateTime.Now.ToString("[HH:mm:ss] "));
+            for (int i = 0; i < indent; i++)
+                cache.Append("\t");
+
+            PEWCoreWriter.WriteLine(cache.Append(text));
+            PEWCoreWriter.Flush();
+            cache.Clear();
+        }
+
+        public void Write(string text)
+        {
+            if (PEWCoreWriter == null)
+                return;
+
+            cache.Append(text);
+        }
+
+        internal void Close()
+        {
+            if (cache.Length > 0)
+                PEWCoreWriter.WriteLine(cache);
+
+            PEWCoreWriter.Flush();
+            PEWCoreWriter.Close();
+            PEWCoreWriter = null;
+        }
+    }
+}
