2a2ba138ccca012e2758c87e81ac17998fa76fa2
FinancedDart1863 <112011102+FinancedDart1863@users.noreply.github.com>
2023-02-20 00:23:18 -0500

    Create PEWCoreModule_HVT stub
    Create PEWCoreModule_HVT stub

---
+++ Data/Scripts/PEWCore/Modules/PEWCoreModule_HVT.cs
@@ -0,0 +1,14 @@
+﻿//© 2023 FinancedDart
+//© 2023 Phobos Engineered Weaponry Group
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEWCore.Modules
+{
+    internal class PEWCoreModule_HVT
+    {
+    }
+}
