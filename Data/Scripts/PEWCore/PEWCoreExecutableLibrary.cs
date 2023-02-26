3f3aed9abcc2c6f1cd8ec3bd406376d7b3534b63
FinancedDart1863 <112011102+FinancedDart1863@users.noreply.github.com>
2023-02-25 19:20:13 -0500

    [PEWCoreExecutableLibrary] Implement PEWCoreExecutableLibrary
    Add a library for forwarding logical core executions to the respective program as per the entered instruction set

---
+++ Data/Scripts/PEWCore/PEWCoreExecutableLibrary.cs
@@ -0,0 +1,34 @@
+﻿//© 2023 FinancedDart
+//© 2023 Phobos Engineered Weaponry Group
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PEWCore.Programs;
+using VRage;
+using VRage.ModAPI;
+
+namespace PEWCore
+{
+    public class PEWCoreExecutableLibrary
+    {
+        public static MyTuple<int, PEWCoreNonVolatileMemory> LoadAndExecuteProgram(IMyEntity entity, string[] instructionSet, PEWCoreNonVolatileMemory nonVolatileMemory)
+        {
+            //A return from exectuable logic will be a zero or nonzero. Zero indicates an error within the exectuable code, while a nonzero indicates a success.
+            //The result is forwarded back to the scheduler so the logic can adjust its execution interval if needed.
+            //Note the returning a positive execution indication will cause the nonvolatile memory to be updated
+            int result = 1;
+            switch (instructionSet[0])
+            {
+                case "Undefined":
+                    return new MyTuple<int, PEWCoreNonVolatileMemory>(0, nonVolatileMemory);
+                case "BasicTimedDispenser":
+                    return PEWCoreProgram_BasicTimedDispenser.execute(entity, instructionSet, nonVolatileMemory);
+                default:
+                    return new MyTuple<int, PEWCoreNonVolatileMemory>(0, nonVolatileMemory);
+            }
+        }
+    }
+}
