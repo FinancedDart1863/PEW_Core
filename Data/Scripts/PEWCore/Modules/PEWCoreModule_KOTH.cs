c2bdb8e356db8ed99f2d9f619594c023b5a90e40
FinancedDart1863 <112011102+FinancedDart1863@users.noreply.github.com>
2023-03-17 01:25:21 -0400

    [PEWCoreModule_KOTH] Stub
    Add stub for PEWCoreModule_KOTH. This module is still very much work in progress.

---
+++ Data/Scripts/PEWCore/Modules/PEWCoreModule_KOTH.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Game.Entities;
+using VRage.Game.ModAPI.Ingame;
+using VRage;
+
+namespace PEWCore.Modules
+{
+    public class PEWCoreModule_KOTH
+    {
+        public static MyTuple<int, PEWCoreNonVolatileMemory> execute(VRage.ModAPI.IMyEntity entity, string[] instructionSet, PEWCoreNonVolatileMemory nonVolatileMemory)
+        {
+            //We need to be careful in program code sections as failures, either with memory accesses or the logic itself, can crash the server. Everything needs to be in a try block.
+
+            if (PEWCoreMain.ConfigData.PEWGeneralConfig.DeveloperMode) { Sandbox.Game.MyVisualScriptLogicProvider.SendChatMessageColored("[Module | KOTH] Execution", VRageMath.Color.White); }
+
+            try
+            {
+                string programName = instructionSet[0];
+                string hillReference = instructionSet[1];
+
+                //KOTH instruction set is only two lines.
+                //Use config for KOTH information
+
+
+                return new MyTuple<int, PEWCoreNonVolatileMemory>(1, nonVolatileMemory);
+            }
+            catch (Exception ex)
+            {
+                return new MyTuple<int, PEWCoreNonVolatileMemory>(0, nonVolatileMemory);
+            }
+        }
+    }
+}
