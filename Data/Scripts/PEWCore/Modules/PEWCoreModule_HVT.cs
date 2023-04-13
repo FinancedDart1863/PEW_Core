7bdcbff42e89bab307d37f376656de481ed15bb7
FinancedDart1863 <112011102+FinancedDart1863@users.noreply.github.com>
2023-04-13 00:22:15 -0400

    [PEWCoreModule_HVT] Implement the HVT module.
    Implement the HVT module. Ignore grids with no functional blocks. Correctly handle grid-groups and grid owner identification. Implement HVT cluster denoising.

--- Data/Scripts/PEWCore/Modules/PEWCoreModule_HVT.cs
+++ Data/Scripts/PEWCore/Modules/PEWCoreModule_HVT.cs
@@ -4,11 +4,190 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.Engine;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+
+using VRage;
+using VRageMath;
+using VRage.Common;
+using VRage.Definitions;
+using VRage.ModAPI;
+using VRage.Serialization;
+using VRage.Game.Entities;
+using VRage.Game.Components;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using System.Text.RegularExpressions;
+using VRage.Scripting;
+using VRage.Game.VisualScripting;
 
 namespace PEWCore.Modules
 {
     internal class PEWCoreModule_HVT
     {
+
+        public static MyTuple<int, MyTuple<PEWCoreVolatileMemory, PEWCoreNonVolatileMemory>> execute(VRage.ModAPI.IMyEntity entity, string[] instructionSet, PEWCoreVolatileMemory volatileMemory, PEWCoreNonVolatileMemory nonVolatileMemory)
+        {
+            if (PEWCoreMain.ConfigData.PEWGeneralConfig.DeveloperMode) { Sandbox.Game.MyVisualScriptLogicProvider.SendChatMessageColored("[Module | HVT] Execution", VRageMath.Color.White); }
+            int ISspecifiedExecInterval = 1;
+            int timeUntilLastExecution = 0; //Time until last execution in seconds
+            int PEWHVT_HVTThreshold = 0;
+
+            try
+            {
+                string programName = instructionSet[0];
+                try { ISspecifiedExecInterval = Int32.Parse(instructionSet[1]); } catch (FormatException) { return new MyTuple<int, MyTuple<PEWCoreVolatileMemory, PEWCoreNonVolatileMemory>>(0, new MyTuple<PEWCoreVolatileMemory, PEWCoreNonVolatileMemory>(volatileMemory, nonVolatileMemory)); }
+                string[] thisProgramMemoryStrings = new string[0] { };
+                bool[] thisProgramMemoryBools = new bool[0] { };
+                int[] thisProgramMemoryInts = new int[1] { 0 };
+
+                MyCubeBlock thisLogicalCore = entity as MyCubeBlock;
+                MyTuple<int, MyTuple<string[], bool[], int[]>> thisProgramNonVolatileMemorySegment = PEWCoreNonVolatileMemory.GetMemorySegment(thisLogicalCore.Name, nonVolatileMemory.NonVolatileMemoryNonShared);
+                if (thisProgramNonVolatileMemorySegment.Item1 == 0)
+                { PEWCoreNonVolatileMemory.SetMemorySegment(thisLogicalCore.Name, nonVolatileMemory.NonVolatileMemoryNonShared, thisProgramMemoryStrings, thisProgramMemoryBools, thisProgramMemoryInts); }
+                else
+                {
+                    thisProgramMemoryStrings = thisProgramNonVolatileMemorySegment.Item2.Item1;
+                    thisProgramMemoryBools = thisProgramNonVolatileMemorySegment.Item2.Item2;
+                    thisProgramMemoryInts = thisProgramNonVolatileMemorySegment.Item2.Item3;
+                }
+
+                timeUntilLastExecution = thisProgramMemoryInts[0];
+                VRage.ModAPI.IMyEntity parent = entity.GetTopMostParent(); //Topmost parent of logical core is the grid on which it is installed.
+
+                if (timeUntilLastExecution >= PEWCoreMain.ConfigData.PEWHVTConfig.PEWHVT_CheckInterval)
+                {
+                    timeUntilLastExecution = 0;
+                    Vector3D position = parent.GetPosition();
+                    BoundingSphereD sphere = new BoundingSphereD(position, 150000f);
+                    List<VRage.ModAPI.IMyEntity> entities = MyAPIGateway.Entities.GetEntitiesInSphere(ref sphere);
+                    List<VRageMath.Vector3D> HVT_coordinates = new List<VRageMath.Vector3D>();
+                    List<VRageMath.Vector3D> HVTentitiesPOS = new List<VRageMath.Vector3D>();
+                    bool HVTViolation = false;
+                    //MyAPIGateway.Utilities.ShowMessage("DBG entities count", entities.Count.ToString());
+
+                    PEWCoreMain.PEWNetworkGPSManager.GPSManagerCleanHVTSystemGPS();
+
+                    List<IMyIdentity> allHistoryPlayersIdentities = new List<IMyIdentity>();
+                    allHistoryPlayersIdentities = new List<IMyIdentity>(); //Start cleanly
+                    MyAPIGateway.Players.GetAllIdentites(allHistoryPlayersIdentities);
+                    for (int i = 0; i < allHistoryPlayersIdentities.Count; i++)
+                    {
+                        PEWCoreMain.PEWNetworkGPSManager.ClientCleanHVTSystemGPS(allHistoryPlayersIdentities[i].PlayerId);
+                    }
+
+                    foreach (IMyEntity foundEntity in entities)
+                    {
+
+                        // Projection or invalid object
+                        if (foundEntity.Physics == null)
+                            continue;
+
+                        // Waypoints and other things that are free of physics
+                        if (!foundEntity.Physics.Enabled)
+                            continue;
+
+                        // Ignore our own grid
+                        if (foundEntity == entity.GetTopMostParent())
+                            break;
+
+
+                        if (foundEntity is IMyCubeGrid)
+                        {
+                            bool adjacentHVTPresent = false;
+                            IMyCubeGrid Igrid = foundEntity as IMyCubeGrid;
+                            MyCubeGrid grid = foundEntity as MyCubeGrid;
+                            List<long> bigowners = grid.BigOwners;
+                            if ((Igrid != null) && (grid != null))
+                            {
+                                IMyGridGroupData gridGroup = MyAPIGateway.GridGroups.GetGridGroup(GridLinkTypeEnum.Physical, Igrid);
+                                List<IMyCubeGrid> gridGroupGrids = new List<IMyCubeGrid>(); //Define list grids within the gridGroup
+                                gridGroup.GetGrids(gridGroupGrids);
+                                int gridGroupTotalBlocks = 0;
+                                foreach (IMyCubeGrid gridX in gridGroupGrids)
+                                {
+                                    MyCubeGrid gridXCast = gridX as MyCubeGrid;
+                                    gridGroupTotalBlocks += gridXCast.BlocksCount;
+                                }
+
+                                if ((grid.BlocksCount > PEWCoreMain.ConfigData.PEWHVTConfig.PEWHVT_HVTThreshold) || (gridGroupTotalBlocks > PEWCoreMain.ConfigData.PEWHVTConfig.PEWHVT_HVTThreshold))
+                                {
+                                    HVTentitiesPOS.Add(grid.Physics.Center);
+                                    HVTViolation = true;
+
+                                    foreach (Vector3D temp in HVT_coordinates)
+                                    {
+                                        if (Vector3D.Distance(temp, grid.Physics.Center) < (double)PEWCoreMain.ConfigData.PEWHVTConfig.PEWHVT_HVTClusterRadius)
+                                        {
+                                            adjacentHVTPresent = true;
+                                        }
+                                    }
+                                    if (!adjacentHVTPresent)
+                                    {
+                                        bool gridBigOwnerSuccess = false;
+                                        try 
+                                        { 
+                                            MyAPIGateway.Utilities.ShowMessage("DBGlongid", grid.BigOwners[0].ToString());
+                                            gridBigOwnerSuccess = true;
+                                        } catch (Exception e) 
+                                        { 
+                                        }
+
+                                        if (gridBigOwnerSuccess)
+                                        {
+                                            IMyFaction gridBigOwnerFaction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(grid.BigOwners[0]);
+                                            string gridBigOwnerFactionTag = gridBigOwnerFaction.Tag;
+                                            //try { IMyFaction playerFaction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(grid.BigOwners[0]); playerFactionTag = playerFaction.Tag; } catch (Exception e) { MyAPIGateway.Utilities.ShowMessage("DBG", "Can't get HVT bigowner faction1"); }
+                                            //MyAPIGateway.Utilities.ShowMessage("DBG", playerFaction.Tag);
+                                            if (gridBigOwnerFactionTag != "SPRT")
+                                            {
+                                                if (gridBigOwnerFactionTag != "")
+                                                {
+                                                    IMyGps temp = MyAPIGateway.Session.GPS.Create("HVT_High Value Target [" + gridBigOwnerFactionTag + "]", "A grid or grid-group above the threshold has been detected at these coordinates! Go attack it!", grid.Physics.Center, true);
+                                                    temp.GPSColor = VRageMath.Color.DarkRed;
+                                                    PEWCoreMain.PEWNetworkGPSManager.GPSManagerAddSystemGPS("HVT_High Value Target", temp);
+                                                }
+                                                else
+                                                {
+                                                    IMyGps temp = MyAPIGateway.Session.GPS.Create("HVT_High Value Target", "A grid or grid-group above the threshold has been detected at these coordinates! Go attack it!", grid.Physics.Center, true);
+                                                    temp.GPSColor = VRageMath.Color.DarkRed;
+                                                    PEWCoreMain.PEWNetworkGPSManager.GPSManagerAddSystemGPS("HVT_High Value Target", temp);
+                                                }
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (HVTViolation)
+                    {
+                        Sandbox.Game.MyVisualScriptLogicProvider.SendChatMessageColored("One or more high value target have been spotted!", VRageMath.Color.Red);
+                    }
+
+                }
+                timeUntilLastExecution++;
+                thisProgramMemoryInts[0] = timeUntilLastExecution;
+
+                PEWCoreNonVolatileMemory.SetMemorySegment(thisLogicalCore.Name, nonVolatileMemory.NonVolatileMemoryNonShared, thisProgramMemoryStrings, thisProgramMemoryBools, thisProgramMemoryInts);
+
+                return new MyTuple<int, MyTuple<PEWCoreVolatileMemory, PEWCoreNonVolatileMemory>>(ISspecifiedExecInterval, new MyTuple<PEWCoreVolatileMemory, PEWCoreNonVolatileMemory>(volatileMemory, nonVolatileMemory));
+            }
+            catch (Exception ex)
+            {
+                return new MyTuple<int, MyTuple<PEWCoreVolatileMemory, PEWCoreNonVolatileMemory>>(0, new MyTuple<PEWCoreVolatileMemory, PEWCoreNonVolatileMemory>(volatileMemory, nonVolatileMemory));
+            }
+
+        }
     }
 }
